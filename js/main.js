410ab4dae86f8b7aed0868ce6214d5eb125d73fc
SaraBarrett <sara.c.mon@gmail.com>
2024-07-31 15:55:07 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// /*alert('hello world');
+// let myLuckyNumber = 16;
+// let myName = 'Sara';
+// const MY_BIRTHDAY = 1986;
+// MY_BIRTHDAY =  1992;
+
+// myName = 'Margarida';
+
+// alert(myName+ ' e o meu número é '+myLuckyNumber+' e o meu ano de nascimento é ' +MY_BIRTHDAY);
+// alert(myName+ ' e o meu número é '+myLuckyNumber+' e o meu ano de nascimento é ' +MY_BIRTHDAY);*/
+
+
+const MESSAGE = "   TASTE THE RAINBOW  ";
+let whisper = MESSAGE.trim().toLowerCase();
+
+console.log(whisper);
